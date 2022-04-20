fb48d8a43b74dd1784e758eb47649cf9ec8a2b18
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-04-20 15:16:51 -0600

    Strategize structure and methodologies of booking class

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -2,7 +2,14 @@ class Customer {
   constructor(customer) {
     this.id = customer.id;
     this.name = customer.name;
+    this.allBookings = [];
+    this.pastBookings = [];
+    this.currentBookings = [];
+    this.futureBookings = [];
   }
+  // addBooking(booking) {
+  //
+  // }
 }
 
 export default Customer;
