b1ba295e98b1888676fdd92fbd548b811a37a23c
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-04-20 11:56:11 -0600

    Add test data file

---
+++ test/data.js
@@ -0,0 +1,15 @@
+const roomsData = [ {"number": 1,
+                      "roomType": "residential suite",
+                      "bidet": true,
+                      "bedSize": "queen",
+                      "numBeds": 1,
+                      "costPerNight": 358.4},
+                      {"number": 2,
+                      "roomType": "suite",
+                      "bidet": false,
+                      "bedSize": "full",
+                      "numBeds": 2,
+                      "costPerNight": 477.38}];
+
+
+export {roomsData};
