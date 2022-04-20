1fc6073741cfded9b6f9e9e2f5b4d0c24335912b
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-04-20 17:03:24 -0600

    Add methods that get all customer bookings and total up the amount spent

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -6,10 +6,30 @@ class Customer {
     this.pastBookings = [];
     this.currentBookings = [];
     this.futureBookings = [];
+    this.amountSpent = 0;
   }
   // addBooking(booking) {
   //
   // }
-}
+  getCustomerBookings(bookings) {
+    bookings.forEach((booking) => {
+      if (this.id === booking.userID) {
+        this.allBookings.push(booking);
+      };
+    });
+  };
+  getTotalSpend(rooms) {
+    const result = this.allBookings.reduce((total, booking) => {
+      rooms.forEach((room) => {
+        if (booking.roomNumber === room.number) {
+          total += room.costPerNight;
+          return total;
+        };
+      });
+      return total;
+    }, 0);
+    return this.amountSpent = result;
+  };
+};
 
 export default Customer;
