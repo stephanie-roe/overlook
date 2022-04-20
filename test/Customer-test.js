2bdfa0632b0fe60ad69c3e5996cabee59fed1f07
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-04-20 17:03:40 -0600

    Add tests to ensure new methods are working

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,14 +1,16 @@
 import chai from "chai";
 const expect = chai.expect;
 import Customer from "../src/classes/Customer";
-import {customersData} from "./data";
+import {customersData, bookingsData, roomsData} from "./data";
 
 describe("Customer", () => {
-  let customer1, customer2;
+  let customer1, customer2, customer3;
 
   beforeEach(() => {
     customer1 = new Customer(customersData[0]);
     customer2 = new Customer(customersData[1]);
+    customer3 = new Customer(customersData[2]);
+
   });
 
   it("should be a function", () => {
@@ -28,18 +30,44 @@ describe("Customer", () => {
     expect(customer1.name).to.equal("Leatha Ullrich");
     expect(customer2.name).to.equal("Rocio Schuster");
   });
+
+  it("should be able to store all of the customer's bookings", () => {
+    customer1.getCustomerBookings(bookingsData);
+    customer2.getCustomerBookings(bookingsData);
+
+    expect(customer1.allBookings[0].roomNumber).to.equal(15);
+    expect(customer1.allBookings[0].id).to.equal("5fwrgu4i7k55hl6sz");
+
+    expect(customer2.allBookings[0].roomNumber).to.equal(24);
+    expect(customer2.allBookings[0].id).to.equal("5fwrgu4i7k55hl6t5");
+  });
+
+  it("should not add a booking to the user's allBookings property if the booking does not belong to the user", () => {
+    customer3.getCustomerBookings(bookingsData);
+
+    expect(customer3.allBookings).to.deep.equal([]);
+  });
+
+  it("should be able total up the amount spent on all bookings", () => {
+    customer1.getCustomerBookings(bookingsData);
+    customer1.getTotalSpend(roomsData);
+
+    expect(customer1.amountSpent).to.equal(358.4);
+  });
 });
 
 // it should have a method that allows the user to book a room
-// it should have a property that contains the customer's all time bookings
+
 // it should have a method to determine if the booking occurred in the past
   // this method will reference current date using conditional logic
   // if it is before the current date, it will be pushed into past bookings array (happy path)
   // if the booking did not occur in the past, it will not pass the conditional logic and nothing will be pushed into the past bookings array/ []. (sad path)
+
 // it should have a method to determine if the booking is currently active (referencing current date)
   // this method will reference the current date using conditional logic
   // if the current date matches the date booked, the booking will be pushed into the current bookings array (happy path)
   // if the current date does not match the date booked, the current bookings array will remain empty/[] (sad path)
+
 // it should have a method to determine if the booking is scheduled for the future (referencing current date)
   // this method will reference current date using conditional logic
   // if the booking is before the current date, the booking will be pushed into the furture bookings array (happy path)
